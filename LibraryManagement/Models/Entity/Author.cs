f4c53e5909907976fccc119181e3fce3b90b65d0
rezatajari <reza.tajari70@gmail.com>
2021-10-09 13:01:09 +0330

    Create Book & Author table

---
+++ LibraryManagement/Models/Entity/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models.Entity
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ LibraryManagement/Models/Entity/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagement.Models.Entity
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set;   }
+
+        public string Year { get; set; }
+
+        public Author Author { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+    }
+}
