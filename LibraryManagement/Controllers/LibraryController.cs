8085a77dc547d47deab03fdd134a7756623617a6
rezatajari <reza.tajari70@gmail.com>
2021-10-25 15:27:40 +0330

    Create Navbar and BookDto.cs

--- LibraryManagement/Controllers/LibraryController.cs
+++ LibraryManagement/Controllers/LibraryController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryManagement.Service;
+using LibraryManagement.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,15 +11,45 @@ namespace LibraryManagement.Web.Controllers
 
     public class LibraryController : Controller
     {
-        private readonly LibraryService _libraryService;
+        private readonly ILibraryService _iLibraryService;
 
-        public LibraryController(LibraryService libraryService)
+        public LibraryController(ILibraryService ilibraryService)
         {
-            _libraryService = libraryService;
+            _iLibraryService = ilibraryService;
         }
 
+        [Route("Library/Index")]
         [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Library/Add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var newBook = new BookDto();
+            return View(newBook);
+        }
+
+        [Route("Library/Add")]
+        [HttpPost]
+        public IActionResult Add(BookDto newBook)
+        {
+            return View();
+        }
+
+        [Route("Library/Edit")]
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [Route("Library/Delete")]
+        [HttpGet]
+        public IActionResult Delete()
         {
             return View();
         }
---
+++ LibraryManagement/Models/BookDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LibraryManagement.Web.Models
+{
+    public class BookDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Price { get; set; }
+
+        public string Author { get; set; }
+    }
+}
