26496f49cb8dc8533bba3fa8359c186eebd47084
rezatajari <reza.tajari70@gmail.com>
2021-12-30 17:44:17 +0330

    Exception Handling

---
+++ LibraryManagement.Service/MessageContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Service
+{
+    public class MessageContract
+    {
+        public MessageContract()
+        {
+
+        }
+
+        public bool IsFaild { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        public List<string> Success { get; set; }
+    }
+
+
+    public class MessageContract<T> : MessageContract
+    {
+        public MessageContract()
+        {
+
+        }
+
+        public T Data { get; set; }
+    }
+}
