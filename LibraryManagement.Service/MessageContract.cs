ad812637f645bd59f4cbe01501c4afe334c8711a
rezatajari <reza.tajari70@gmail.com>
2022-01-29 16:24:53 +0330

    Add paging class

--- LibraryManagement.Service/MessageContract.cs
+++ LibraryManagement.Service/MessageContract.cs
@@ -10,26 +10,44 @@ namespace LibraryManagement.Service
     {
         public MessageContract()
         {
-            Errors = new List<string>();
-            Message = null;
-            IsSuccess = false;
+
+        }
+        public MessageContract(bool success, string message = null, List<string> errors = null)
+        {
+            IsSuccess = success;
+            Message = message;
+            Errors = errors;
         }
 
         public bool IsSuccess { get; set; }
 
-        public List<string> Errors { get; set; }
-
         public string Message { get; set; }
+
+        public List<string> Errors { get; set; } = new List<string>();
     }
 
 
     public class MessageContract<T> : MessageContract
     {
-        public MessageContract()
+
+        public MessageContract() : base(success: false)
         {
 
         }
 
+        public MessageContract(T data) : base(success: true)
+        {
+            Data = data;
+        }
+
+        public MessageContract(T data, PagedResult paged) : base(success: true)
+        {
+            Data = data;
+            Paged = paged;
+        }
+
         public T Data { get; set; }
+
+        public PagedResult Paged { get; set; }
     }
 }
---
+++ LibraryManagement.Service/PagedResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Service
+{
+    public class PagedResult
+    {
+        public PagedResult()
+        {
+
+        }
+
+        public PagedResult(int pageNumber, int pageSize, long total)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalItems = total;
+            TotalPages = (int)Math.Ceiling((double)total / PageSize);
+        }
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 2;
+        public long TotalItems { get; set; } = 0;
+        public int TotalPages { get; set; } = 0;
+    }
+}
